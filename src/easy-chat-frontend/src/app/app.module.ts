9749d184b8ccfe75944be104b84dcb9498691413
marcnee <marcnee@users.noreply.github.com>
2020-10-08 19:10:46 +0200

    feat: add ContentComponent

--- src/easy-chat-frontend/src/app/app.module.ts
+++ src/easy-chat-frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
