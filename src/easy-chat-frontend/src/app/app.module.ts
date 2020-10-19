41c69e7d1e38d4f8b2c5bcb1092e44e335368fc7
marcnee <marcnee@users.noreply.github.com>
2020-10-19 14:39:18 +0200

    feat: add ChatBarComponent

--- src/easy-chat-frontend/src/app/app.module.ts
+++ src/easy-chat-frontend/src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContentComponent } from './components/content/content.component';
+import { ChatBarComponent } from './components/chat-bar/chat-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    ChatBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/easy-chat-frontend/src/app/components/chat-bar/chat-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ec-chat-bar',
+  templateUrl: './chat-bar.component.html',
+  styleUrls: ['./chat-bar.component.css']
+})
+export class ChatBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
