34efd52ae16896f010a8a05556d78dc853565053
marcnee <marcnee@users.noreply.github.com>
2020-10-26 15:12:11 +0100

    feat: Use ChatMessage Model

--- src/easy-chat-frontend/src/app/app.module.ts
+++ src/easy-chat-frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,6 +9,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ContentComponent } from './components/content/content.component';
 import { ChatBarComponent } from './components/chat-bar/chat-bar.component';
 import { CurrentlyTypingBarComponent } from './components/currently-typing-bar/currently-typing-bar.component';
+
+
+const APP_LOCALE_ID = 'de-CH';
+
+import locale_de_ch from '@angular/common/locales/de-CH';
+registerLocaleData(locale_de_ch);
+
 
 @NgModule({
   declarations: [
@@ -22,7 +30,11 @@ import { CurrentlyTypingBarComponent } from './components/currently-typing-bar/c
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: APP_LOCALE_ID
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
