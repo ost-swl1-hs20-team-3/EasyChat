7599f6c2f901d42eb16ccf4d7e30bf5b86919fc4
SirCrypto <55431896+SirCrypto@users.noreply.github.com>
2020-11-06 12:49:53 +0100

    feat: add scroll @History

--- src/easy-chat-frontend/src/app/components/content/content.component.ts
+++ src/easy-chat-frontend/src/app/components/content/content.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ec-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent implements OnInit {
+export class ContentComponent implements OnInit, AfterViewChecked {
+
+  @ViewChild('scrollable') private myScrollContainer: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToElement();
+  }
+
+  scrollToElement(): void {
+    this.myScrollContainer.nativeElement.scroll({
+      top: this.myScrollContainer.nativeElement.scrollHeight,
+      behavior: 'smooth'
+    });
   }
 
 }
