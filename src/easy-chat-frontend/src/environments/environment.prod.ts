fb768d07a8f2018482c7ac65d8d49768e473310f
marcnee <marcnee@users.noreply.github.com>
2020-11-23 16:14:39 +0100

    fix: remove cors-anywhere

--- src/easy-chat-frontend/src/environments/environment.prod.ts
+++ src/easy-chat-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // https://cors-anywhere.herokuapp.com/ to prevent Mixed content error (github.io has SSL - Heroku not)
-  SOCKET_ENDPOINT: 'https://cors-anywhere.herokuapp.com/http://easy-chat-backend.herokuapp.com/',
+  SOCKET_ENDPOINT: 'http://easy-chat-backend.herokuapp.com/',
   FEATURE_FLAGS: {
   }
 };
