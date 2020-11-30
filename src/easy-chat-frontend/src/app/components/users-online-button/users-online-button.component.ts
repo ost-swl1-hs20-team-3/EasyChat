a47fdb90e86a8d7a1a7b1e5573b7cf78fc3f8ac6
marcnee <marcnee@users.noreply.github.com>
2020-11-30 17:20:38 +0100

    feat: add server part and popover-list

--- src/easy-chat-frontend/src/app/components/users-online-button/users-online-button.component.ts
+++ src/easy-chat-frontend/src/app/components/users-online-button/users-online-button.component.ts
@@ -1,36 +1,33 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { SocketioService } from 'src/app/services/socketio.service';
-import { UserService } from 'src/app/services/user.service';
+import { ChatService } from '../../services/chat.service';
+import { SocketioService } from '../../services/socketio.service';
 
 @Component({
   selector: 'ec-users-online-button',
   templateUrl: './users-online-button.component.html',
   styleUrls: ['./users-online-button.component.css']
 })
-export class UsersOnlineButtonComponent implements OnInit, OnDestroy {
+export class UsersOnlineButtonComponent implements OnInit {
 
-  private reservedUsernamesSubscription = new Subscription();
-
-  public onlineUsernames: string[] = [];
 
   constructor(
-    private socketioService: SocketioService
-  ) {
-    this.reservedUsernamesSubscription = this.socketioService.getReservedUsernames().subscribe((msg) => {
-      this.onlineUsernames = msg.getReservedUsernames();
-    });
-  }
+    private socketioService: SocketioService,
+    private chatService: ChatService
+  ) { }
 
   public ngOnInit(): void {
   }
 
-  public ngOnDestroy(): void {
-    this.reservedUsernamesSubscription.unsubscribe();
-  }
-
   public isConnected(): boolean {
     return this.socketioService.isConnected();
+  }
+
+  public getNumberOfOnlineUsers(): number {
+    return this.chatService.onlineUserNames.length;
+  }
+
+  public getOnlineUsernames(): Array<string> {
+    return this.chatService.onlineUserNames;
   }
 
 }
