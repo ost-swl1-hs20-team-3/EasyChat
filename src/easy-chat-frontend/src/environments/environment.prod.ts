fedea6c6e24f314c89a3824274e79a90c46d2959
marcnee <marcnee@users.noreply.github.com>
2020-12-06 12:10:22 +0100

    fix: FE - use localhost for DEV and https Backend for prod

--- src/easy-chat-frontend/src/environments/environment.prod.ts
+++ src/easy-chat-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   MESSAGE_LIMIT: 15,
-  SOCKET_ENDPOINT: 'http://easy-chat-backend.herokuapp.com/',
+  SOCKET_ENDPOINT: 'https://easy-chat-backend.herokuapp.com/',
   FEATURE_FLAGS: {
   }
 };
